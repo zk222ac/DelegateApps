18f1b25dd68a81a69e9b5a9438b54e8017ac235a
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-07 18:36:38 +0100

    Final Release Delegate

--- DelegateApps/Program.cs
+++ DelegateApps/Program.cs
@@ -6,16 +6,80 @@ using System.Threading.Tasks;
 
 namespace DelegateApps
 {
+    // Declare Delegate 
+    public delegate int Calculator(int a , int b);
     class Program
     {
+        
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            /*
+            // call add method through delegate object reference
+            Calculator calAdd = new Calculator(Add);
+            var sum = calAdd(2, 4);
+            Console.WriteLine("Sum of a and b is:" +sum);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // call Sub method through delegate object reference
+            Calculator calSub = new Calculator(Sub);
+            var sub = calSub(2, 4);
+            Console.WriteLine("Subtract of a and b is:" + sub);
+           
+            // call Mul method through delegate object reference
+            Calculator calMul = new Calculator(Mul);
+            var mul = calMul(2, 4);
+            Console.WriteLine("Subtract of a and b is:" + mul);
+
+            // call Div method through delegate object reference
+            Calculator calDiv = new Calculator(Div);
+            var div = calDiv(2, 4);
+            Console.WriteLine("Division of a and b is:" + div);
+
+            // Pass delegate as a Parameter 
+            Modules(Add , 1, 2);
+            Modules(Mul , 1, 2);
+            Modules(Div, 1, 2);
+            Modules(Div, 1, 2);
+            */
+
+            // Multi Delegate ............ call method simultaneously  
+            Calculator cal = Add;
+            cal += Sub;
+            cal += Div;
+            cal += Mul;
+            // invoke delegate or call
+            Console.WriteLine(cal(1, 2));
+            // alternative technique
+            //cal.Invoke(1, 2);
+            Console.WriteLine("Hello Delegate World!");
+            Console.ReadKey();
+        }
+
+        static int Add(int a, int b)
+        {
+            Console.WriteLine("Add:" + a + b);
+            return a + b;
+        }
+        static int Mul(int a, int b)
+        {
+            Console.WriteLine("Mul:" + a * b);
+            return a * b;
+        }
+        static int Sub(int a, int b)
+        {
+            Console.WriteLine("Sub:" + (a - b));
+            return  a - b;
+        }
+        static int Div(int a, int b)
+        {
+            Console.WriteLine("Div:" + a / b);
+            return a / b;
+        }
+
+        //Pass Delegate as a parameter 
+        static void Modules( Calculator cal , int a , int b)
+        {
+            // delegate invoke method here
+            cal(a, b);
         }
     }
 }
